a29cbec8eda00e00ee2ead9dd20dcd1e3f090ca1
f4G0Ld <vasia.kharitonov@gmail.com>
2025-12-26 13:48:47 +0300

    deploy bugfix 7

---
+++ server/ApiLogger.ts
@@ -0,0 +1,58 @@
+import type { ValidationError } from "elysia";
+
+const prettyLog = process.env.NODE_ENV !== "production";
+
+export function ApiLogger({
+	request,
+	path,
+	error,
+	code,
+}: {
+	request: Request;
+	path: string;
+	error?: Readonly<Error> | Readonly<ValidationError> | unknown;
+	code?:
+		| (
+				| number
+				| "UNKNOWN"
+				| "VALIDATION"
+				| "NOT_FOUND"
+				| "PARSE"
+				| "INTERNAL_SERVER_ERROR"
+				| "INVALID_COOKIE_SIGNATURE"
+		)
+		| (string & {});
+}) {
+	const { method, body } = request;
+
+	if (!code) {
+		console.log({
+			path,
+			method,
+		});
+		return;
+	}
+
+	switch (code) {
+		case "VALIDATION":
+			prettyLog &&
+				console.error(
+					`\n  Validation not passed \n ${method.toUpperCase()}:${path}`,
+				);
+			console.error({
+				error,
+			});
+			prettyLog && console.error(`\n ${JSON.stringify(body)}`);
+			return;
+		case "UNKNOWN":
+		case "INTERNAL_SERVER_ERROR":
+			prettyLog &&
+				console.error(`\n  Server error \n ${method.toUpperCase()}:${path}`);
+			console.error({
+				path: `${method.toUpperCase()}:${path}`,
+				error,
+			});
+			prettyLog && console.error("End server error \n");
+			return;
+	}
+}
--- server/app.ts
+++ server/app.ts
@@ -2,11 +2,12 @@ import { fileRouter } from "@/server/files/fileServices";
 import { userRouter } from "@/server/files/userServices";
 import Elysia from "elysia";
 import { auth } from "./files/auth";
+import { ApiLogger } from "./ApiLogger";
 
 export const app = new Elysia({
 	name: "app",
 	prefix: "/api",
-})
+}).onError(ApiLogger)
 
 	.use(fileRouter)
 	.use(userRouter)
